cdccbf248ca58e9d4ede012378e7926394763f7e
Matthew Kelleher <Matthew.kelleher10@comcast.net>
2024-04-02 12:45:48 -0400

    Added file to hold UI color theme

---
+++ decentrapass/library/Colors.js
@@ -0,0 +1,18 @@
+/**************************************************************************
+  File: Colors.js
+  Author: Matthew Kelleher
+  Description: Holds all UI color variables
+**************************************************************************/
+
+const Colors = {
+  text: "#e0e6f4",
+  backgroundLight: "#020659",
+  backgroundDark: "#010440",
+  primary: "#026873",
+  secondary: "#C3F8FD",
+  tertiary: "#00A6FB",
+  accentDark: "#055902",
+  accentLight: "#55D976",
+};
+
+export default Colors;
