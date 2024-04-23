c5c17e3e9548bae665a9e904eef5b6782342c0af
Matthew Kelleher <Matthew.kelleher10@comcast.net>
2024-04-23 11:17:39 -0400

    Added options to settings

--- decentrapass/components/FancyButton.js
+++ decentrapass/components/FancyButton.js
@@ -8,9 +8,9 @@ import Colors from "../library/Colors";
   Description: Button Component
 **************************************************************************/
 
-const FancyButton = ({ children, onClick, active = false, size = "big" }) => {
+const FancyButton = ({ children, onClick, active = false, size = "big", danger = false }) => {
   return (
-    <Button onClick={onClick} active={active} size={size}>
+    <Button onClick={onClick} active={active} size={size} danger={danger}>
       {children}
     </Button>
   );
@@ -28,19 +28,27 @@ const Button = styled.button`
   display: block;
   border: none;
   font-weight: 700;
-  background-image: ${(props) =>
-    props.active
-      ? `linear-gradient(
+  background-image: ${props =>
+    props.active === true ? 
+    `linear-gradient(
       45deg,
       ${Colors.accentDark},
       ${Colors.accentLight}
     )`
-      : `linear-gradient(
-      45deg,
-      ${Colors.primary} 0%,
-      ${Colors.tertiary} 33%,
-      ${Colors.accentLight} 66%,
-      ${Colors.accentDark} 100%
+    : props.danger === true ? 
+    `linear-gradient(
+    45deg,
+    red,
+    darkred,
+    black
+    )`
+    :
+    `linear-gradient(
+    45deg,
+    ${Colors.primary} 0%,
+    ${Colors.tertiary} 33%,
+    ${Colors.accentLight} 66%,
+    ${Colors.accentDark} 100%
     )`};
   cursor: pointer;
   &:hover {
--- decentrapass/pages/settings.js
+++ decentrapass/pages/settings.js
@@ -6,8 +6,16 @@ import FancyButton from "@/components/FancyButton";
 import Colors from "@/library/Colors";
 import Navbar from "@/components/Navbar";
 import Background from "@/components/Background";
+import { useRouter } from "next/router";
 // React
 import { useState, useEffect } from "react";
+// Contract
+import { useAddress, useStorage, useSigner } from "@thirdweb-dev/react";
+import DecentraPassPasswordsABI from "@/contracts/abi/DecentraPassPasswordsABI";
+import { PASSWORDS_CONTRACT_ADDRESS } from "@/global-values";
+import DecentraPassNotesABI from "@/contracts/abi/DecentraPassNotesABI";
+import { NOTES_CONTRACT_ADDRESS } from "@/global-values";
+import { ethers } from "ethers";
 
 /**************************************************************************
   File: settings.js
@@ -16,6 +24,99 @@ import { useState, useEffect } from "react";
 **************************************************************************/
 
 export default function Settings() {
+  const router = useRouter();
+  const signer = useSigner();
+  const storage = useStorage();
+  const userAddress = useAddress();
+
+  async function removeAllPasswords() {
+    const asyncFunc = async () => {
+      if (!signer) {
+        return;
+      }
+      if (!userAddress) {
+        return;
+      }
+      // Get all passwords
+      const passwordList = [];
+      const contract = new ethers.Contract(
+        PASSWORDS_CONTRACT_ADDRESS,
+        DecentraPassPasswordsABI,
+        signer
+      );
+      try {
+        const passwordURIs = await contract.getAllPasswordURI();
+        const dataPromises = passwordURIs.map(async (uri) => {
+          try {
+            const data = await storage.download(uri);
+            const metadataResponse = await fetch(data.url);
+            const metadata = await metadataResponse.json();
+            passwordList.push(metadata);
+            return metadata;
+          } catch (err) {
+            console.log(err);
+          }
+        });
+        const results = await Promise.all(dataPromises);
+      } catch (err) {
+        console.log(err);
+      }
+      // Delete all passwords
+      passwordList.map(async (item) => {
+        const tx = await contract.deletePasswordURI(item.id);
+        await tx.wait();
+      });
+    };
+    asyncFunc();
+  }
+
+  async function removeAllNotes() {
+    const asyncFunc = async () => {
+      if (!signer) {
+        return;
+      }
+      if (!userAddress) {
+        return;
+      }
+      // Get all notes
+      const noteList = [];
+      const contract = new ethers.Contract(
+        NOTES_CONTRACT_ADDRESS,
+        DecentraPassNotesABI,
+        signer
+      );
+      try {
+        const noteURIs = await contract.getAllNoteURI();
+        const dataPromises = noteURIs.map(async (uri) => {
+          try {
+            const data = await storage.download(uri);
+            const metadataResponse = await fetch(data.url);
+            const metadata = await metadataResponse.json();
+            noteList.push(metadata);
+            return metadata;
+          } catch (err) {
+            console.log(err);
+          }
+        });
+        const results = await Promise.all(dataPromises);
+      } catch (err) {
+        console.log(err);
+      }
+      // Delete all notes
+      noteList.map(async (note) => {
+        const tx = await contract.deleteNoteURI(note.id);
+        await tx.wait();
+      });
+    };
+    asyncFunc();
+  }
+
+  // Run when switch user button clicked
+  function SwitchUser() {
+    location.reload();
+    router.push("/loading");
+  }
+
   return (
     <>
       <Head>
@@ -34,12 +135,28 @@ export default function Settings() {
                 {/* Profile Settings */}
                 <SectionTitle>Profile Settings</SectionTitle>
                 <ProfileForm>
+                  <Label>Current User:</Label>
+                  <Info>{userAddress}</Info>
                 </ProfileForm>
+                <FancyButton onClick={() => SwitchUser()}>
+                  Switch User
+                </FancyButton>
               </Card>
             </SettingsSection>
             <SettingsSection>
               <Card>
-                <SectionTitle>Other Settings</SectionTitle>
+                <SectionTitle>Danger Zone</SectionTitle>
+                <ButtonContainer>
+                  <FancyButton
+                    danger={true}
+                    onClick={() => removeAllPasswords()}
+                  >
+                    Remove All Passwords
+                  </FancyButton>
+                  <FancyButton danger={true} onClick={() => removeAllNotes()}>
+                    Remove All Notes
+                  </FancyButton>
+                </ButtonContainer>
               </Card>
             </SettingsSection>
           </Container>
@@ -81,22 +198,18 @@ const SectionTitle = styled.h3`
 const ProfileForm = styled.form`
   display: flex;
   flex-direction: column;
+  margin-bottom: 1vw;
 `;
 
-const InputLabel = styled.label`
+const Label = styled.h1`
   font-size: 1.25vw;
   margin-bottom: 0.5vw;
 `;
 
-const InputField = styled.input`
-  margin-bottom: 1vw;
-  padding: 0.5vw;
-  width: 20vw;
-  font-size: 1vw;
-  border-radius: 0.5vw;
-  border: none;
-  transition: 0.25s;
-  &:active {
-    transform: scale(0.95);
-  }
+const Info = styled.p``;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 1vw;
 `;
